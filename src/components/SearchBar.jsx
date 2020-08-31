aabb4373e0432fd97939505e5e8462566a2b306f
mivchal <mivchal@hotmail.com>
2020-08-31 21:26:21 +0200

    search to func component with hooks

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,39 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
-class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            term: '',
-        }
+const SearchBar = props => {
+    const [term, setTerm] = useState('')
+
+    const onInputChange = (event) => {
+        setTerm(event.target.value)
     }
 
-    onInputChange = (event) => {
-        this.setState({term: event.target.value})
-    }
-
-    onFormSubmit = (event) => {
+    const onFormSubmit = (event) => {
         event.preventDefault();
-        this.props.onFormSubmit(this.state.term)
+        props.onFormSubmit(term)
     }
 
-    render() {
-        return (
-            <div className="ui segment search-bar">
-                <form className="ui form" onSubmit={this.onFormSubmit}>
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input
-                            type="text"
-                            value={this.state.term}
-                            onChange={this.onInputChange}
-                        />
-                    </div>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div className="ui segment search-bar">
+            <form className="ui form" onSubmit={onFormSubmit}>
+                <div className="field">
+                    <label>Video Search</label>
+                    <input
+                        type="text"
+                        value={term}
+                        onChange={onInputChange}
+                    />
+                </div>
+            </form>
+        </div>
+    )
 }
-
 export default SearchBar;
